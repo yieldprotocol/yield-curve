9b735f1a953a9e983d5dab9ad7006e89cbae7e61
Rishabh Varshney <it.is.rishabh@gmail.com>
2020-10-16 11:29:39 -0400

    updated logo

--- src/components/logotype.js
+++ src/components/logotype.js
@@ -4,11 +4,10 @@ import { Link } from 'gatsby'
 const Logotype = ({ company }) => (
   <Link className="inline-block text-white font-semibold link font-display" title={company} to="/">
     <img
-      className="inline-block align-middle mr-4 w-6 lg:w-8"
+      className="inline-block align-middle mr-4 w-16"
       alt={company}
-      src="/logo-white.svg"
+      src="/type-white.svg"
     />
-    <strong className="hidden md:inline-block align-middle">Yield</strong>
   </Link>
 )
 
